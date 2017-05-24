d9156389316a6739e0ca174a773f8e47b521b834
Abhi Trivedi <abhitrivedi64@yahoo.com>
2017-05-23 19:29:46 -0700

    Skeleton for SuperBlock.java

---
+++ ThreadOS/SuperBlock.java
@@ -0,0 +1,19 @@
+public class SuperBlock {
+    public int totalBlocks;
+    public int totalInodes;
+    public int freeList;
+
+    public SuperBlock(int diskSize) {
+
+    }
+
+    public int sync() {}
+
+    public int getFreeBlocks() {
+
+    }
+
+    public int returnBlock(int blockID) {
+
+    }
+}
