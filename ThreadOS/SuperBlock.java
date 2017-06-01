64cd116ac5fe5315e3966846338235b52556bf1b
Abhi Trivedi <abhitrivedi64@yahoo.com>
2017-06-01 11:26:25 -0700

    SuperBlock should be done, requires iNode dependency so will wait 2 test

--- ThreadOS/SuperBlock.java
+++ ThreadOS/SuperBlock.java
@@ -8,8 +8,8 @@ public class SuperBlock {
     public SuperBlock(int diskSize) {
         byte[] superBlock = new byte[512];
 
-
         SysLib.rawread(0, superBlock);
+
         totalBlocks = SysLib.bytes2int(superBlock, 0);
         totalInodes = SysLib.bytes2int(superBlock, 4);
         freeList = SysLib.bytes2int(superBlock, 8);
@@ -24,23 +24,84 @@ public class SuperBlock {
 
     }
 
-    //sync basically writes totalBlocks, totalIndoes, and freeList to the disk.
+    //sync function, basically writes totalBlocks, totalIndoes, and freeList to the disk.
     public void sync() {
         byte buffer = new byte[512];
+
         SysLib.int2bytes(totalBlocks, buffer, 0);
         SysLib.int2bytes(totalInodes, buffer, 4);
         SysLib.int2bytes(freeList, buffer, 8);
+
         SysLib.rawwrite(buffer, 0);
 
     }
 
-
+    //getBlocks basically returns the first free block from the free list. The top block is dequeued from the free list
     public int getFreeBlocks() {
+        byte[] data = new byte[512];
+
+        SysLib.rawread(freeList, data); //Reading the free list block
+        int temp = freeList;
+
+        freeList = SysLib.bytes2int(temp, 0);
+
+        return temp;
+    }
+
+
+    //format function, basically clears the disk of all that data and all the instance variables are set to default values
+    //and returned to the disk
+    public void format(int numFiles)
+    {
+        totalInodes = numFiles;
+        Inode empty = null;
+
+        for(int i = 0; i < totalInodes; i++) {
+            empty = new Inode();
+            empty.flag = 0;
+            empty.toDisk((short) i);
+
+        }
+
+        if(numFiles % 16 == 0) {
+            freeList = numFiles / 16 + 1;
+        } else {
+            freeList = numFiles / 16 + 2;
+        }
+
+        for(int i = freeList; i < 1000 - 1; i++) {
+            byte data = new byte[512];
+
+            for(int j = 0; j < 512; j++) {
+                data[j] = 0;
+            }
+
+            SysLib.int2bytes(i + 1; data, 0);
+            SysLib.rawwrite(i, data);
+        }
+
+        for(int j = 0; j < 512; j++) {
+
+            byte data = new byte[512];
+            data[j] = 0;
+        }
+        SysLib.int2bytes(-1, data, 0);
+        SysLib.rawwrite(1000 - 1, data);
+
+        sync();
 
 
     }
 
+    //returnBlock function, adds the new freed block to the front of free list based on its blockID
     public int returnBlock(int blockID) {
+        byte[] data = new byte[512];
+
+        SysLib.int2bytes(freeList, data, 0);
+        SysLib.rawwrite(blockID, data);
+
+        freeList = blockID;
 
     }
+
 }
